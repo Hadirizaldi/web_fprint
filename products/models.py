d13bd02067e26f4ab684531e1c9f16c6a0052ba5
Hadi Rizaldi R <hadirizaldi.hr@gmail.com>
2023-12-06 22:40:17 +0700

    make utils.py for generate password format md5

--- products/models.py
+++ products/models.py
@@ -6,9 +6,15 @@ from django.db import models
 class Category(models.Model):
   category_name = models.CharField(max_length=100)
 
+  def __str__(self):
+    return self.category_name
+  
 # table status
 class Status(models.Model):
   status_name = models.CharField(max_length=50)
+
+  def __str__(self):
+    return self.status_name
 
 # table product
 class Product(models.Model):
@@ -16,3 +22,6 @@ class Product(models.Model):
   price = models.DecimalField(decimal_places=2, max_digits=10)
   category_id = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products_in_category")
   status_id = models.ForeignKey(Status, on_delete=models.CASCADE, related_name="products_with_status")
+
+  def __str__(self):
+    return self.product_name
