8381b5a6fb1aa04237371c4cb45a5bf81f69ff7c
Hadi Rizaldi R <hadirizaldi.hr@gmail.com>
2023-12-06 16:46:35 +0700

    set up database use mysql

--- web_fprint/settings.py
+++ web_fprint/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products'
+    'products',
+    'web_fprint'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'web_fprint.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'web_fprint',
+        'USER': 'megatron',
+        'PASSWORD': 'Megatron-111213',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
