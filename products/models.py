8c8b1e0cb3219a98c8064260607ac67d7fede250
Hadi Rizaldi R <hadirizaldi.hr@gmail.com>
2023-12-06 20:42:57 +0700

    create model for products app (Category, Product, Status)

--- products/models.py
+++ products/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+# table category
+class Category(models.Model):
+  category_name = models.CharField(max_length=100)
+
+# table status
+class Status(models.Model):
+  status_name = models.CharField(max_length=50)
+
+# table product
+class Product(models.Model):
+  product_name = models.CharField(max_length=50) 
+  price = models.DecimalField(decimal_places=2, max_digits=10)
+  category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+  status_id = models.ForeignKey(Status, on_delete=models.CASCADE)
