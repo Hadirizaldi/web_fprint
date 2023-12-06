a5d94c059938243973f60a26837306f0ba967303
Hadi Rizaldi R <hadirizaldi.hr@gmail.com>
2023-12-06 21:15:49 +0700

    edit app products for model

--- products/models.py
+++ products/models.py
@@ -14,5 +14,5 @@ class Status(models.Model):
 class Product(models.Model):
   product_name = models.CharField(max_length=50) 
   price = models.DecimalField(decimal_places=2, max_digits=10)
-  category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
-  status_id = models.ForeignKey(Status, on_delete=models.CASCADE)
+  category_id = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products_in_category")
+  status_id = models.ForeignKey(Status, on_delete=models.CASCADE, related_name="products_with_status")
