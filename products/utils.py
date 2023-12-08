7e42603c549cb30b58d79a52d1b15da76b007af7
Hadi Rizaldi R <hadirizaldi.hr@gmail.com>
2023-12-08 12:35:49 +0700

    add def generate_username in utils.py and edit def generate_password_md5

--- products/utils.py
+++ products/utils.py
@@ -1,16 +1,24 @@
 import hashlib
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # for generate password format md5
 def generate_password_md5() :
-  today = datetime.now()
-  current_date = today.strftime("%d");
-  current_month = today.strftime("%m");
-  current_year = today.strftime("%y")
-  password_string = f"bisacoding-{current_date}-{current_month}-{current_year}"
+  time_data = datetime.now() + timedelta(hours=1)
+  date = time_data.strftime("%d")
+  month = time_data.strftime("%m")
+  year = time_data.strftime("%y")
+  password_string = f"bisacoding-{date}-{month}-{year}"
   password_md5 = hashlib.md5(password_string.encode()).hexdigest()
 
-  
   return password_md5
 
+def generate_username():
+  time_data = datetime.now() + timedelta(hours=1)
+  date = time_data.strftime("%d")
+  month = time_data.strftime("%m")
+  year = time_data.strftime("%y")
+  hour = time_data.strftime("%H")
+  username = f"testprogrammer{date}{month}{year}C{hour}"
+
+  return username
 
